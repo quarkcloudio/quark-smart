bb660531beee5a6fb5632077f6e9b9af81dd9382
tangtanglove <dai_hang_love@126.com>
2023-02-16 15:40:06 +0800

    fix: 修正配置文件

--- config/app.go
+++ config/app.go
@@ -5,28 +5,28 @@ import (
 )
 
 type AppConfig struct {
-	Name    string // 应用名称
 	Version string // 应用版本
+	Name    string // 应用名称
+	Env     string // 项目环境
 	Host    string // 服务地址
 	Key     string // 令牌加密key，如果设置绝对不可泄漏
-	Env     string // 项目环境
 }
 
 // APP配置信息
 var App = &AppConfig{
 
+	// 应用版本
+	Version: "1.0.0",
+
 	// 应用名称
 	Name: env.Get("APP_NAME", "QuarkSmart").(string),
 
-	// 应用版本
-	Version: "1.0.0",
+	// 项目环境
+	Env: env.Get("APP_ENV").(string),
 
 	// 服务地址
 	Host: env.Get("APP_HOST", "127.0.0.1:3000").(string),
 
 	// 令牌加密key，如果设置绝对不可泄漏
 	Key: env.Get("APP_KEY").(string),
-
-	// 项目环境
-	Env: env.Get("APP_ENV").(string),
 }
