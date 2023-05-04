c8a07c4426cc784d2d21ccbdd1c18e20801b8bd3
tangtanglove <dai_hang_love@126.com>
2023-05-04 17:43:47 +0800

    fix: 修复登录bug

--- internal/admin/login/index.go
+++ internal/admin/login/index.go
@@ -1,7 +1,6 @@
 package login
 
 import (
-	"bytes"
 	"time"
 
 	"github.com/dchest/captcha"
@@ -10,8 +9,8 @@ import (
 	"github.com/quarkcms/quark-go/pkg/builder"
 	"github.com/quarkcms/quark-go/pkg/builder/template/adminlogin"
 	"github.com/quarkcms/quark-go/pkg/hash"
-	"github.com/quarkcms/quark-go/pkg/msg"
 	"github.com/quarkcms/quark-smart/config"
+	"gorm.io/gorm"
 )
 
 type Index struct {
@@ -40,57 +39,42 @@ func (p *Index) Init() interface{} {
 	p.SubTitle = config.Admin.SubTitle
 
 	// 登录后跳转地址
-	p.Redirect = "/index?api=/api/admin/dashboard/index/index"
+	p.Redirect = "/layout/index?api=/api/admin/dashboard/index/index"
 
 	return p
 }
 
-// 验证码ID
-func (p *Index) CaptchaId(ctx *builder.Context) interface{} {
-
-	return ctx.JSON(200, msg.Success("获取成功", "", map[string]string{
-		"captchaId": captcha.NewLen(4),
-	}))
-}
-
-// 生成验证码
-func (p *Index) Captcha(ctx *builder.Context) interface{} {
-	id := ctx.Param("id")
-	writer := bytes.Buffer{}
-	captcha.WriteImage(&writer, id, 110, 38)
-	ctx.Write(writer.Bytes())
-
-	return nil
-}
-
 // 登录方法
-func (p *Index) Handle(ctx *builder.Context) interface{} {
+func (p *Index) Handle(ctx *builder.Context) error {
 	loginRequest := &LoginRequest{}
 	if err := ctx.BodyParser(loginRequest); err != nil {
-		return ctx.JSON(200, msg.Error(err.Error(), ""))
+		return ctx.JSONError(err.Error())
 	}
 	if loginRequest.CaptchaId == "" || loginRequest.Captcha == "" {
-		return ctx.JSON(200, msg.Error("验证码不能为空", ""))
+		return ctx.JSONError("验证码不能为空")
 	}
 
 	verifyResult := captcha.VerifyString(loginRequest.CaptchaId, loginRequest.Captcha)
 	if !verifyResult {
-		return ctx.JSON(200, msg.Error("验证码错误", ""))
+		return ctx.JSONError("验证码错误")
 	}
 	captcha.Reload(loginRequest.CaptchaId)
 
 	if loginRequest.Username == "" || loginRequest.Password == "" {
-		return ctx.JSON(200, msg.Error("用户名或密码不能为空", ""))
+		return ctx.JSONError("用户名或密码不能为空")
 	}
 
 	adminInfo, err := (&model.Admin{}).GetInfoByUsername(loginRequest.Username)
 	if err != nil {
-		return ctx.JSON(200, msg.Error(err.Error(), ""))
+		if err == gorm.ErrRecordNotFound {
+			return ctx.JSONError("用户不存在")
+		}
+		return ctx.JSONError(err.Error())
 	}
 
 	// 检验账号和密码
 	if !hash.Check(adminInfo.Password, loginRequest.Password) {
-		return ctx.JSON(200, msg.Error("用户名或密码错误", ""))
+		return ctx.JSONError("用户名或密码错误")
 	}
 
 	config := ctx.Engine.GetConfig()
@@ -101,13 +85,11 @@ func (p *Index) Handle(ctx *builder.Context) interface{} {
 
 	// 获取token字符串
 	tokenString, err := token.SignedString([]byte(config.AppKey))
+	if err != nil {
+		return ctx.JSONError(err.Error())
+	}
 
-	return ctx.JSON(200, msg.Success("登录成功", "", map[string]string{
+	return ctx.JSONOk("登录成功", "", map[string]string{
 		"token": tokenString,
-	}))
-}
-
-// 退出方法
-func (p *Index) Logout(ctx *builder.Context) interface{} {
-	return ctx.JSON(200, msg.Success("退出成功", "", ""))
+	})
 }
