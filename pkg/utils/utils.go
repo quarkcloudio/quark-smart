466f0e61786bb3ac56e9757b21e6c2ebcbb510de
tangtanglove <dai_hang_love@126.com>
2023-05-04 14:03:53 +0800

    fix: 修复bug

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,20 +6,36 @@ import (
 
 // 获取文件路径
 func GetFilePath(id interface{}) string {
+	if id == nil {
+		return ""
+	}
+
 	return (&model.File{}).GetPath(id)
 }
 
 // 获取多文件路径
 func GetFilePaths(id interface{}) []string {
+	if id == nil {
+		return nil
+	}
+
 	return (&model.File{}).GetPaths(id)
 }
 
 // 获取图片路径
 func GetPicturePath(id interface{}) string {
+	if id == nil {
+		return ""
+	}
+
 	return (&model.Picture{}).GetPath(id)
 }
 
 // 获取多图片路径
 func GetPicturePaths(id interface{}) []string {
+	if id == nil {
+		return nil
+	}
+
 	return (&model.Picture{}).GetPaths(id)
 }
