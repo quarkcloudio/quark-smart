7e8f90ec92977e6b619f9eed2b70dbb7dc1fd11b
Zero <y95013585@163.com>
2023-10-16 15:51:23 +0800

    modify: 完善代码

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -39,3 +39,25 @@ func GetPicturePaths(id interface{}) []string {
 
 	return (&model.Picture{}).GetPaths(id)
 }
+
+// 获取配置
+func GetConfig(key string) string {
+	return (&model.Config{}).GetValue(key)
+}
+
+// 获取域名
+func GetDomain() string {
+
+	http := ""
+	domain := (&model.Config{}).GetValue("WEB_SITE_DOMAIN")
+	ssl := (&model.Config{}).GetValue("SSL_OPEN")
+	if domain != "" {
+		if ssl == "1" {
+			http = "https://"
+		} else {
+			http = "http://"
+		}
+	}
+
+	return http + domain
+}
