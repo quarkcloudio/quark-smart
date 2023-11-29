58da56d9856d321cfa5b75668270ba3c1a781e03
tangtanglove <dai_hang_love@126.com>
2023-11-29 13:09:35 +0800

    fix: 优化脚本

--- service.sh
+++ service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$1"x=="start"x ]; then
+if [ "$1"x == "start"x ]; then
     bash "./scripts/start_service.sh"
 fi
 
-if [ "$1"x=="stop"x ]; then
+if [ "$1"x == "stop"x ]; then
     bash "./scripts/stop_service.sh"
 fi
