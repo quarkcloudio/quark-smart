78602bc24d663545fbb5e75592505b716ac2b861
tangtanglove <dai_hang_love@126.com>
2025-03-21 09:13:12 +0800

    chore: 适配新版本

--- internal/app/admin/engine/upload/file.go
+++ internal/app/admin/engine/upload/file.go
@@ -1,19 +1,13 @@
 package upload
 
 import (
-	"encoding/json"
-	"reflect"
-
 	"github.com/quarkcloudio/quark-go/v3"
-	"github.com/quarkcloudio/quark-go/v3/model"
-	"github.com/quarkcloudio/quark-go/v3/service"
-	"github.com/quarkcloudio/quark-go/v3/template/admin/upload"
+	"github.com/quarkcloudio/quark-go/v3/app/admin/uploads"
 	"github.com/quarkcloudio/quark-smart/v2/config"
-	"github.com/quarkcloudio/quark-smart/v2/internal/dto/response"
 )
 
 type File struct {
-	upload.Template
+	uploads.File
 }
 
 // 初始化
@@ -29,92 +23,4 @@ func (p *File) Init(ctx *quark.Context) interface{} {
 	p.SavePath = config.App.UploadFileSavePath
 
 	return p
-}
-
-// 上传前回调
-func (p *File) BeforeHandle(ctx *quark.Context, fileSystem *quark.FileSystem) (*quark.FileSystem, *quark.FileInfo, error) {
-	fileHash, err := fileSystem.GetFileHash()
-	if err != nil {
-		return fileSystem, nil, err
-	}
-
-	fileInfo, err := service.NewAttachmentService().GetInfoByHash(fileHash)
-	if err != nil {
-		return fileSystem, nil, err
-	}
-
-	if fileInfo.Id != 0 {
-		var extra map[string]interface{}
-		if fileInfo.Extra != "" {
-			_ = json.Unmarshal([]byte(fileInfo.Extra), &extra)
-		}
-		fileInfo := &quark.FileInfo{
-			Name:  fileInfo.Name,
-			Size:  fileInfo.Size,
-			Ext:   fileInfo.Ext,
-			Path:  fileInfo.Path,
-			Url:   fileInfo.Url,
-			Hash:  fileInfo.Hash,
-			Extra: extra,
-		}
-		return fileSystem, fileInfo, err
-	}
-
-	return fileSystem, nil, err
-}
-
-// 上传完成后回调
-func (p *File) AfterHandle(ctx *quark.Context, result *quark.FileInfo) error {
-	driver := reflect.
-		ValueOf(ctx.Template).
-		Elem().
-		FieldByName("Driver").
-		String()
-
-	// 重写url
-	if driver == quark.LocalStorage {
-		result.Url = service.NewAttachmentService().GetFilePath(result.Url)
-	}
-	adminInfo, err := service.NewAuthService(ctx).GetAdmin()
-	if err != nil {
-		return ctx.CJSONError(err.Error())
-	}
-
-	extra := ""
-	if result.Extra != nil {
-		extraData, err := json.Marshal(result.Extra)
-		if err == nil {
-			extra = string(extraData)
-		}
-	}
-
-	// 插入数据库
-	id, err := service.NewAttachmentService().InsertGetId(model.Attachment{
-		Source: "ADMIN",
-		Uid:    adminInfo.Id,
-		Name:   result.Name,
-		Type:   "FILE",
-		Size:   result.Size,
-		Ext:    result.Ext,
-		Path:   result.Path,
-		Url:    result.Url,
-		Hash:   result.Hash,
-		Extra:  extra,
-		Status: 1,
-	})
-	if err != nil {
-		return ctx.CJSONError(err.Error())
-	}
-
-	return ctx.CJSONOk("登录成功", response.UploadResp{
-		Id:          id,
-		ContentType: result.ContentType,
-		Ext:         result.Ext,
-		Hash:        result.Hash,
-		Name:        result.Name,
-		Path:        result.Path,
-		Size:        result.Size,
-		Url:         result.Url,
-		Extra:       result.Extra,
-	})
 }
--- internal/app/tool/engine/upload/image.go
+++ internal/app/tool/engine/upload/image.go
@@ -1,19 +1,13 @@
 package upload
 
 import (
-	"encoding/json"
-	"reflect"
-
 	"github.com/quarkcloudio/quark-go/v3"
-	"github.com/quarkcloudio/quark-go/v3/model"
-	"github.com/quarkcloudio/quark-go/v3/service"
-	"github.com/quarkcloudio/quark-go/v3/template/tool/upload"
+	appupload "github.com/quarkcloudio/quark-go/v3/app/tool/upload"
 	"github.com/quarkcloudio/quark-smart/v2/config"
-	"github.com/quarkcloudio/quark-smart/v2/internal/dto/response"
 )
 
 type Image struct {
-	upload.Template
+	appupload.Image
 }
 
 // 初始化
@@ -29,87 +23,4 @@ func (p *Image) Init(ctx *quark.Context) interface{} {
 	p.SavePath = config.App.UploadImageSavePath
 
 	return p
-}
-
-// 上传前回调
-func (p *Image) BeforeHandle(ctx *quark.Context, fileSystem *quark.FileSystem) (*quark.FileSystem, *quark.FileInfo, error) {
-	fileHash, err := fileSystem.GetFileHash()
-	if err != nil {
-		return fileSystem, nil, err
-	}
-
-	imageInfo, err := service.NewAttachmentService().GetInfoByHash(fileHash)
-	if err != nil {
-		return fileSystem, nil, err
-	}
-
-	if imageInfo.Id != 0 {
-		var extra map[string]interface{}
-		if imageInfo.Extra != "" {
-			_ = json.Unmarshal([]byte(imageInfo.Extra), &extra)
-		}
-
-		fileInfo := &quark.FileInfo{
-			Name:  imageInfo.Name,
-			Size:  imageInfo.Size,
-			Ext:   imageInfo.Ext,
-			Path:  imageInfo.Path,
-			Url:   imageInfo.Url,
-			Hash:  imageInfo.Hash,
-			Extra: extra,
-		}
-		return fileSystem, fileInfo, err
-	}
-
-	return fileSystem, nil, err
-}
-
-// 上传完成后回调
-func (p *Image) AfterHandle(ctx *quark.Context, result *quark.FileInfo) error {
-	driver := reflect.
-		ValueOf(ctx.Template).
-		Elem().
-		FieldByName("Driver").String()
-
-	// 重写url
-	if driver == quark.LocalStorage {
-		result.Url = service.NewAttachmentService().GetPath(result.Url)
-	}
-
-	extra := ""
-	if result.Extra != nil {
-		extraData, err := json.Marshal(result.Extra)
-		if err == nil {
-			extra = string(extraData)
-		}
-	}
-
-	// 插入数据库
-	id, err := service.NewAttachmentService().InsertGetId(model.Attachment{
-		Name:   result.Name,
-		Type:   "IMAGE",
-		Size:   result.Size,
-		Ext:    result.Ext,
-		Path:   result.Path,
-		Url:    result.Url,
-		Hash:   result.Hash,
-		Extra:  extra,
-		Status: 1,
-	})
-
-	if err != nil {
-		return ctx.JSONError(err.Error())
-	}
-
-	return ctx.JSONOk("上传成功", response.UploadResp{
-		Id:          id,
-		ContentType: result.ContentType,
-		Ext:         result.Ext,
-		Hash:        result.Hash,
-		Name:        result.Name,
-		Path:        result.Path,
-		Size:        result.Size,
-		Url:         result.Url,
-		Extra:       result.Extra,
-	})
 }
